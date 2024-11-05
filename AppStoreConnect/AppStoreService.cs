03f7de92b836255e92d3f0e440bcb56754fe7d74
nupatrickxu <xu.pe@northeastern.edu>
2024-11-04 19:41:37 -0500

    Set up basic project structure and register necessary services to main service.

---
+++ AppStoreConnect/AppStoreService.cs
@@ -0,0 +1,20 @@
+﻿
+using AppStoreConnect.Options;
+using Microsoft.Extensions.Options;
+
+namespace AppStoreConnect;
+
+public class AppStoreService : IAppStoreSevice
+{
+    private readonly AppStoreOptions _options;
+
+    public AppStoreService(IOptions<AppStoreOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public Task<string> CreateSubscriptionGroup(string appId, string referenceName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AppStoreConnect/Options/AppStoreOptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace AppStoreConnect.Options;
+
+public class AppStoreOptions : IOptions<AppStoreOptions>
+{
+    /// <summary>
+    /// Key Id of the ios application registered on the dev account
+    /// </summary>
+    public required string KeyId { get; set; }
+
+    /// <summary>
+    /// Issuer Id of the apple dev account
+    /// </summary>
+    public required string IssuerId { get; set; }
+
+    /// <summary>
+    /// Path to the private key file
+    /// </summary>
+    public required string PrivateKeyPath { get; set; }
+
+    /// <summary>
+    /// Set expiration period for the generated token
+    /// Default: 20 mins
+    /// </summary>
+    public int ExpirationMinutes { get; set; } = 20;
+
+    AppStoreOptions IOptions<AppStoreOptions>.Value => this;
+}
